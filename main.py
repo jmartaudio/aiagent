76c2d9d114ebfeb7249c1abf2be0baaf0b1c0353
Jason Martinez <jason@bigpictureranch.com>
2025-06-18 09:21:46 -0700

    all inital features implemented

--- main.py
+++ main.py
@@ -29,11 +29,33 @@ def main():
     if verbose:
         print(f"User prompt: {user_prompt}\n")
 
-    messages = [
-        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
-    ]
+    messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
+    count = 0
+    while count < 20:
+        response = generate_content(client, messages, verbose)
 
-    generate_content(client, messages, verbose)
+        for candidate in response.candidates:
+            messages.append(candidate.content)
+
+        if response.function_calls:
+            print("Processing function calls...")
+            for function_call_part in response.function_calls:
+                function_call_result = call_function(function_call_part, verbose)
+
+                if (
+                    not function_call_result.parts
+                    or not function_call_result.parts[0].function_response
+                ):
+                    raise Exception("empty function call result")
+                if verbose:
+                    print(
+                        f"-> {function_call_result.parts[0].function_response.response}"
+                    )
+                messages.append(function_call_result.parts[0])
+            count += 1
+        else:
+            print(f"Final response: {response.text}")
+            break
 
 
 def generate_content(client, messages, verbose):
@@ -44,27 +66,12 @@ def generate_content(client, messages, verbose):
             tools=[available_functions], system_instruction=system_prompt
         ),
     )
+
     if verbose:
         print("Prompt tokens:", response.usage_metadata.prompt_token_count)
         print("Response tokens:", response.usage_metadata.candidates_token_count)
 
-    if not response.function_calls:
-        return response.text
-
-    function_responses = []
-    for function_call_part in response.function_calls:
-        function_call_result = call_function(function_call_part, verbose)
-        if (
-            not function_call_result.parts
-            or not function_call_result.parts[0].function_response
-        ):
-            raise Exception("empty function call result")
-        if verbose:
-            print(f"-> {function_call_result.parts[0].function_response.response}")
-        function_responses.append(function_call_result.parts[0])
-
-    if not function_responses:
-        raise Exception("no function responses generated, exiting.")
+    return response
 
 
 if __name__ == "__main__":
